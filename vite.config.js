551d9c6fd6f37e45ec54143a879102489395bc30
Apple <apple@Azixxs-MacBook-Pro.local>
2025-03-05 14:09:07 +0100

    Fix vite-plugin-html issue

--- vite.config.js
+++ vite.config.js
@@ -2,11 +2,12 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 import tailwindcss from '@tailwindcss/vite'
 import { createHtmlPlugin } from 'vite-plugin-html'
+import vitePluginHtml from 'vite-plugin-html';
 import sitemap from 'vite-plugin-sitemap'
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [react() , tailwindcss(),
+  plugins: [react() , vitePluginHtml(), tailwindcss(),
     createHtmlPlugin({
       inject: {
         data: {
